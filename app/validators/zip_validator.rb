fd18d488225131ee58901fdc9b3efb90588940c3
Amanda Sorribes <amanda@amsorribes.com>
2015-05-14 12:28:03 +0200

    Change postal code requirement to 5 digits, starting by 28 (28xxx)

--- app/validators/zip_validator.rb
+++ app/validators/zip_validator.rb
@@ -2,7 +2,7 @@ class ZipValidator < RegexValidator
   def validate_each(record, attribute, value)
     default_message = "#{value} #{I18n.t('errors.messages.invalid_zip')}"
 
-    regex = /\A\d{5}(-\d{4})?\z/
+    regex = /\A28\d{3}\z/
 
     regex_validate_each(regex, default_message, record, attribute, value)
   end
